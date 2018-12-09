805ee172f612b0239e2bcee6950a51010339cbbd
seymakara <seymakara88@gmail.com>
2018-12-08 17:25:46 -0800

    Complete CookieParser functionality

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,12 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  req['session'] = {};
+  req['session']['hash'] = {};
+  if (Object.keys(req.cookies).length === 0) {
+    res.cookies['shortlyid'] = 'fdsaf';
+  }
+  next();
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  let cookies = {};
+  if (req.headers['cookie']) {
+    let stringOfCookies = req.headers['cookie'];
+    let keyValuePairs = stringOfCookies.split('; ');
+    let splitOnEqual = keyValuePairs.map(pair => {
+      return pair.split('=');
+    });
+    splitOnEqual.forEach(pair => {
+      cookies[pair[0]] = pair[1];
+    });
+  }
+  req.cookies = cookies;
+  next();
 };
 
 module.exports = parseCookies;
